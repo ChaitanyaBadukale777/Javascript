ef53549d990b6400f7f66e24e64efee9d36c1555
Chaitanya Badukale <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-06-18 08:21:16 +0000

    update datatype summary

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -5,16 +5,16 @@
 const score = 100
 const scoreValue = 100.3
 
-// const isLoggedIn = false
-// const outsodeTemp = null
-// let userEmail;
+const isLoggedIn = false
+const outsodeTemp = null
+let userEmail;
 
-// const id = Symbol('123')
-// const anotherid = Symbol('123')
+const id = Symbol('123')
+const anotherid = Symbol('123')
 
-// console.log(id === anotherid); // false, because symbols are unique
+console.log(id === anotherid); // false, because symbols are unique
 
-// const bigNumber = 123456789012345678901234n; // BigInt
+const bigNumber = 123456789012345678901234n; // BigInt
 
 // Reference Type (Non-Primitive)
 
