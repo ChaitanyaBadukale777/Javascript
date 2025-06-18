4658f3ac53131d60b60516bfd05be8e0c5871a05
Chaitanya Badukale <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-06-18 08:42:03 +0000

    datatype-summary

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -33,3 +33,32 @@ const myFunction = function(){
 console.log(typeof heros); // object
 console.log(typeof scoreValue); // object
 console.log(typeof myFunction); // function
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+// Types of Memory in JavaScript
+ 
+// Stack (Primitive), Heap (Non Primitive)
+
+// 1. Stack Memory: Stores primitive data types (String, Number, Boolean, Null, Undefined, Symbol, BigInt) and references to objects.
+// 2. Heap Memory: Stores objects, arrays, and functions. It is a larger memory space used for dynamic memory allocation.
+
+let myYoutubename = "realdexter.com"
+
+let anothername = myYoutubename
+anothername = "dexter.com"
+
+console.log(myYoutubename); // realdexter.com
+console.log(anothername); // dexter.com
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "chaitanya@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email); 
