38f8e8e00b70a922016f131f19b0a1d2e89543db
Chaitanya Badukale <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-07-18 08:15:21 +0000

    updated

--- Basics/05_strings.js
+++ Basics/05_strings.js
@@ -3,22 +3,27 @@ const repoCount = 28
 
 // console.log(name +repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Dexter')
+// const gameName = new String('Dexter')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('x'));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('x'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
-const anotherString = gameName.slice(-4, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-4, 4)
+// console.log(anotherString);
 
+const url = "https://www.infyspark.com/infy%20spark"
+
+// console.log(url.replace('%20', '-'))
+
+console.log(url.includes('infyspark'))
 
