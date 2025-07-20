87451a7650f20bbdfcc169c2df03ff4af3c05096
Chaitanya Badukale <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-07-20 10:34:07 +0000

    changes

--- Basics/07_date_and_time.js
+++ Basics/07_date_and_time.js
@@ -1,4 +1,7 @@
 // Dates
 
-let myDate = new Date()
-console.log(myDate.toString());
+let myDate = new Date() // Create a new Date object with the current date and time
+console.log(myDate.toString()); // Current date and time
+console.log(myDate.toDateString()); // Current date
+console.log(myDate.toTimeString()); // Current time
+console.log(myDate.toISOString()); // ISO format
