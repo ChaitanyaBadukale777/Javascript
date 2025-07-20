6518119e599c554a7176a34a169f27a2e594f941
Chaitanya Badukale <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-07-20 10:47:15 +0000

    updated

--- Basics/07_date_and_time.js
+++ Basics/07_date_and_time.js
@@ -5,3 +5,16 @@ console.log(myDate.toString()); // Current date and time
 console.log(myDate.toDateString()); // Current date
 console.log(myDate.toTimeString()); // Current time
 console.log(myDate.toISOString()); // ISO format
+
+console.log(typeof myDate); // Check the type of myDate
+
+let myCreatedDate = new Date(2023, 0, 22) // Create a new Date object for January 22, 2023
+// console.log(myCreatedDate.toLocaleString()); // Date in string format
+
+// let myCreated_Date = new Date("01-13-2025") // Create a new Date object from a string
+// console.log(myCreated_Date.toLocaleString()); // Date in string format
+
+let myTimeStamp = Date.now() // Get the current timestamp in milliseconds
+// console.log(myTimeStamp); // Current timestamp
+
+console.log(Math.floor(Date.now()/1000)); // Current timestamp in seconds
